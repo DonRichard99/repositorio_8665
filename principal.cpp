48d304d40b8391d4dbf4d9c5ff58fafe9b446be8
Don Richard <ricardojaramillo99@hotmail.com>
2020-05-28 16:46:35 -0500

    segundo y tercer commit

---
+++ principal.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fibonaci(int n)
+{
+//insercion de codigo
+int a=0;b=1;
+return 0;
+}
+
+int factorial(intp)
+{
+//insercion de codigo
+int factorial =1;
+return 0;
+}
+main()
+{
+//funcion principal
+int numero;
+}
+
